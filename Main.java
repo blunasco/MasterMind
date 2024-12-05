77b7b457fc9c61394f2816e939be79fc7495b7b5
Belle Lunasco <68099842+blunasco@users.noreply.github.com>
2024-12-04 20:56:48 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,21 +2,15 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
         int rounds = 0;
-        List<String> roundHistoryList = new ArrayList<>();
-
         String numberToGuess = createNumberToGuess();
         int[] numberToGuessArray = storeNumberToGuess(numberToGuess);
-
-
+        List<String> roundHistoryList = new ArrayList<>();
 
         while (true) {
             String userGuess = getUserGuess(rounds);
@@ -24,69 +18,57 @@ public class Main {
             int numberOfCorrectPositions = checkCorrectPosition(numberToGuessArray, userGuessArray);
             int numberOfCorrectNumbers = checkCorrectNumbers(numberToGuessArray, userGuessArray);
             rounds++;
+
             if (numberOfCorrectNumbers == 4 && numberOfCorrectPositions == 4) {
                 System.out.println("You win.");
-                showGameHistory(roundHistoryList);
                 break;
             }
-// TODO CHANGE ROUNDS to 10
+
             if (rounds == 10) {
                 showGameHistory(roundHistoryList);
                 System.out.println("ROUND "+ rounds + " User Guess: " + userGuess + "\n \t \t Game over. The correct answer was " + numberToGuess + ".");
                 break;
             }
-
             storeGameHistory(rounds, userGuess, numberOfCorrectPositions, numberOfCorrectNumbers, roundHistoryList);
             showGameHistory(roundHistoryList);
-
         }
     }
 
     //METHOD TO FETCH NUMBER TO GUESS
     public static String createNumberToGuess() {
         HttpClient client = HttpClient.newHttpClient();
-        String apiUrl = "https://www.random.org/integers/?num=4&min=1&max=7&col=4&base=10&format=plain&rnd=new";
+        String apiUrl = "https://www.random.org/integers/?num=4&min=0&max=7&col=4&base=10&format=plain&rnd=new";
         try {
+            //Create a get request
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(apiUrl))// URI: address HTTP Client:GPS
-                    .GET() // HTTP GET method
+                    .uri(URI.create(apiUrl))
+                    .GET()
                     .build();
-
+            //Store the response
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            // Remove spaces from the response
+            //Create a String to manipulate the response (removing spaces)
             String numberToGuess = response.body().trim().replaceAll("\\s+", "");
-
+            //Return the manipulated String
             return numberToGuess;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Error."; // Return an error string in case of failure
         }
+        catch (Exception e) {
+            System.out.println("Error failed to fetch from API. Exception message: " + e + "\nUsing default Number to Guess.");
+        }
+        return "0000";
     }
 
     //METHOD TO STORE NUMBER TO GUESS
     public static int[] storeNumberToGuess(String numberToGuess) {
         int[] numberToGuessArray = new int[4];
-        char createChar = numberToGuess.charAt(0);
 
-        if (!Character.isDigit(createChar)) {
-            System.out.println("Error. Try running the game again.");
-            System.exit(0);
+        for (int i = 0; i < numberToGuess.length(); i++) {
+            numberToGuessArray[i] = Character.getNumericValue(numberToGuess.charAt(i));
         }
-
-        else {
-            for (int i = 0; i < 4; i++) {
-                numberToGuessArray[i] = Character.getNumericValue(numberToGuess.charAt(i));
-            }
-            //TODO DELETE
-            System.out.println("The code is:" + Arrays.toString(numberToGuessArray));
-        }
-            return numberToGuessArray;
+        return numberToGuessArray;
     }
 
     // METHOD TO CAPTURE USER GUESS AND VALIDATES THAT THE GUESS IS A VALID INPUT
-    // To Do: add validator that the intake is not a string
     public static String getUserGuess(int rounds) {
         int remainingRounds = 10 - rounds;
         System.out.println("Guess the 4 digit code. Remaining rounds left:" + remainingRounds);
@@ -94,12 +76,11 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
         String userGuess = keyboard.nextLine();
 
-            while(userGuess.length()!=4){
+        while (userGuess.length() != 4 || !userGuess.matches(("[0-7]"))) {
             System.out.println("Invalid guess. Try again.");
             userGuess = keyboard.nextLine();
         }
         return userGuess;
-
     }
 
     //METHOD TO STORE USER GUESS
@@ -109,8 +90,6 @@ public class Main {
         for (int i = 0; i < 4; i++) {
             userGuessArray[i] = Character.getNumericValue(userGuess.charAt(i));
         }
-
-        System.out.println(Arrays.toString(userGuessArray));
         return userGuessArray;
     }
 
@@ -131,9 +110,9 @@ public class Main {
         int correctNumbers = 0;
         boolean[] usedNumberToGuess = new boolean[numberToGuessArray.length];
 
-        for (int i = 0; i < 4; i++) { //user guess array l
-            for (int j = 0; j < 4; j++) {
-                if (!usedNumberToGuess[j] && userGuessArray[i] == numberToGuessArray[j]) {
+        for (int i = 0; i < 4; i++) { //compares userGuessArray with every number in numberToGuessArray
+            for (int j = 0; j < 4; j++) { //numberToGuess
+                if (!usedNumberToGuess[j] && (userGuessArray[i] == numberToGuessArray[j])) {
                     correctNumbers++;
                     usedNumberToGuess[j] = true;
                     break;
@@ -143,13 +122,7 @@ public class Main {
         return correctNumbers;
     }
 
-    //METHOD FOR GAME HISTORY
-    public static void showGameHistory(List<String> roundHistoryList) {
-        for (int i = 0; i < roundHistoryList.size(); i++) {
-            System.out.println(roundHistoryList.get(i));
-        }
-    }
-
+    //METHOD TO CREATE AND STORE GAME HISTORY
     public static void storeGameHistory(int rounds, String userGuess, int numberOfCorrectPositions, int numberOfCorrectNumbers, List<String> roundHistoryList) {
         String roundHistory;
 
@@ -159,6 +132,13 @@ public class Main {
             roundHistory = "ROUND " + rounds + " User guess: " + userGuess + " Correct position: " + numberOfCorrectPositions + " Correct Number: " + numberOfCorrectNumbers;
         }
         roundHistoryList.add(roundHistory);
+    }
+
+    //METHOD TO SHOW GAME HISTORY
+    public static void showGameHistory(List<String> roundHistoryList) {
+        for (int i = 0; i < roundHistoryList.size(); i++) {
+            System.out.println(roundHistoryList.get(i));
+        }
     }
 }
 
